50fc58be106f70685b75d7452ddb580a3cd77299
Aleksei Iupinov <a.yupinov@gmail.com>
2016-03-02 17:29:19 +0100

    remove thrust

--- src/gromacs/ewald/th-a.cuh
+++ src/gromacs/ewald/th-a.cuh
@@ -2,6 +2,7 @@
 #define GMX_EWALD_TH_A_H
 
 #include "gromacs/utility/real.h"
+#include "gromacs/math/gmxcomplex.h"
 
 const int TH = 32;
 enum th_id
@@ -22,6 +23,9 @@ enum th_id
   TH_ID_F,
   TH_ID_I,
   TH_ID_DTHX, TH_ID_DTHY, TH_ID_DTHZ,
+  TH_ID_BSP_MOD_X, TH_ID_BSP_MOD_Y, TH_ID_BSP_MOD_Z,
+  TH_ID_ENERGY,
+  TH_ID_VIRIAL,
 
   TH_ID_END
 };
@@ -33,7 +37,13 @@ enum th_loc
 
 real *th_a(th_id id, int thread, int size = -1, th_loc loc = TH_LOC_END);
 int *th_i(th_id id, int thread, int size = -1, th_loc loc = TH_LOC_END);
+t_complex *th_c(th_id id, int thread, int size = -1, th_loc loc = TH_LOC_END);
 
 void th_cpy(void *dest, void *src, int size, th_loc dest_loc);
+
+real *th_a_cpy(th_id id, int thread, void *src, int size, th_loc loc = TH_LOC_END);
+t_complex *th_c_cpy(th_id id, int thread, void *src, int size, th_loc loc = TH_LOC_END);
+
+//yupinov warn o nwrong param
 
 #endif
