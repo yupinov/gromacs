f70ae8363c60337b7c4101a7d3143f33ee68c90a
Aleksei Iupinov <a.yupinov@gmail.com>
2016-04-21 18:38:10 +0200

    some checks that will be useful later for multirank

--- src/gromacs/ewald/pme-cuda.cu
+++ src/gromacs/ewald/pme-cuda.cu
@@ -53,6 +53,8 @@ void pme_gpu_init(gmx_pme_gpu_t **pmeGPU, gmx_pme_t *pme, const gmx_hw_info_t *h
         const int PMEGPURank = 0; //yupinov !
         FILE *fp = NULL; //yupinov pass this
         char gpu_err_str[STRLEN];
+        assert(hwinfo->gpu_info.gpu_dev);
+        assert(gpu_opt->dev_use);
         (*pmeGPU)->deviceInfo = &hwinfo->gpu_info.gpu_dev[gpu_opt->dev_use[PMEGPURank]];
         if (!init_gpu(fp, PMEGPURank, gpu_err_str, &hwinfo->gpu_info, gpu_opt))
             gmx_fatal(FARGS, "could not select GPU %d for PME rank %d\n", (*pmeGPU)->deviceInfo->id, PMEGPURank);
