9aa9c83607563a8a9215d9868f6b0ab90332525b
Aleksei Iupinov <iupinov@math.tu-berlin.de>
2016-05-29 20:12:40 +0200

    .

--- data/thesis/cutoff.py
+++ data/thesis/cutoff.py
@@ -3,18 +3,23 @@ import matplotlib.pyplot as plt
 import math
 
 # Compute the x and y coordinates for points on a sine curve
-x = np.arange(0.1, 5, 0.01)
+x = np.arange(0.01, 5, 0.01)
 y = 1 / x
-beta = 1
+beta = 0.7
 ydir = [math.erf(beta * i) / i for i in x]
 yrec = [math.erfc(beta * i) / i for i in x]
 
+plt.rcParams.update({'font.size': 18})
+
 # Plot the points using matplotlib
-plt.plot(x, y, label='Total')
-plt.plot(x, ydir, label='Direct')
-plt.plot(x, yrec, label='Reciprocal')
+plt.plot(x, y, label='Total (1 / r)')
+plt.plot(x, ydir, label='Direct (erf('+str(beta)+' * r) / r)')
+plt.plot(x, yrec, label='Reciprocal (erfc('+str(beta)+' * r) / r)')
 plt.xlabel('r (distance)')
-plt.title('Ewald decomposition')
+plt.xlim(xmin=0, xmax=4)
+plt.ylim(ymin=0, ymax=4)
+#plt.ylabel('r (distance)')
+# plt.title('Dependency')
 plt.legend()
 #plt.show()  # You must call plt.show() to make graphics appear.
 
