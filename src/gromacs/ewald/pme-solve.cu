56735b4f48783c51528dc16019096eb971fc231f
Aleksei Iupinov <a.yupinov@gmail.com>
2016-08-18 15:00:07 +0200

    missing headers

--- src/gromacs/ewald/pme-solve.cu
+++ src/gromacs/ewald/pme-solve.cu
@@ -1,3 +1,39 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright (c) 2016, by the GROMACS development team, led by
+ * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
+ * and including many others, as listed in the AUTHORS file in the
+ * top-level source directory and at http://www.gromacs.org.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * http://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at http://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out http://www.gromacs.org.
+ */
+#include "gmxpre.h"
+
 #include "gromacs/utility/gmxassert.h"
 #include "pme-cuda.cuh"
 #include "pme-gpu.h"   //?
@@ -216,7 +252,7 @@ __global__ void pme_solve_kernel
             __shared__ real virialAndEnergyShared[enerVirSize * blockSize];
             // 3.5k smem per block - a serious limiter!
 
-            // 7-thread reduction in shared memory inspired by reduce_force_j_generic
+            /*  a 7-thread reduction in shared memory inspired by reduce_force_j_generic */
             if (threadLocalId < blockSize)
             {
                 virialAndEnergyShared[threadLocalId + 0 * blockSize] = virxx;
@@ -262,8 +298,8 @@ __global__ void pme_solve_kernel
 
             /* A naive reduction */
             /*
-            if (threadLocalId < blockSize)
-            {
+               if (threadLocalId < blockSize)
+               {
                 virialAndEnergyShared[sizing * threadLocalId + 0] = virxx;
                 virialAndEnergyShared[sizing * threadLocalId + 1] = viryy;
                 virialAndEnergyShared[sizing * threadLocalId + 2] = virzz;
@@ -271,23 +307,23 @@ __global__ void pme_solve_kernel
                 virialAndEnergyShared[sizing * threadLocalId + 4] = virxz;
                 virialAndEnergyShared[sizing * threadLocalId + 5] = viryz;
                 virialAndEnergyShared[sizing * threadLocalId + 6] = energy;
-            }
-            __syncthreads();
-#pragma unroll
-            for (unsigned int stride = 1; stride < blockSize; stride <<= 1)
-            {
+               }
+               __syncthreads();
+               #pragma unroll
+               for (unsigned int stride = 1; stride < blockSize; stride <<= 1)
+               {
                 if ((threadLocalId % (stride << 1) == 0))
                 {
-#pragma unroll
+               #pragma unroll
                     for (int i = 0; i < sizing; i++)
                         virialAndEnergyShared[sizing * threadLocalId + i] += virialAndEnergyShared[sizing * (threadLocalId + stride) + i];
                 }
                 __syncthreads();
-            }
-            if (threadLocalId < sizing)
-            {
+               }
+               if (threadLocalId < sizing)
+               {
                 atomicAdd(virialAndEnergy + threadLocalId, virialAndEnergyShared[threadLocalId]);
-            }
+               }
              */
         }
     }
