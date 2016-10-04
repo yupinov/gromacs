49a0f9a672c684c14f78058f60279dc810e71a1d
Berk Hess <hess@kth.se>
2016-10-04 11:34:21 +0200

    Align PaddedRVecVector to 4096 bytes
    Change-Id: I59a439b196ef7aebdc93fe536286910f62ac2013
    
    Conflicts:
    	src/gromacs/utility/alignedallocator.cpp

--- src/gromacs/math/paddedvector.h
+++ src/gromacs/math/paddedvector.h
@@ -45,6 +45,7 @@
 
 #include <vector>
 
+#include "gromacs/utility/alignedallocator.h"
 #include "gromacs/math/vectypes.h"
 
 namespace gmx
@@ -53,7 +54,7 @@ namespace gmx
 /*! \brief Temporary definition of a type usable for SIMD-style loads of RVec quantities.
  *
  * \todo This vector is not padded yet, padding will be added soon */
-using PaddedRVecVector = std::vector<gmx::RVec>;
+using PaddedRVecVector = std::vector < gmx::RVec, gmx::AlignedAllocator < gmx::RVec>>;
 
 } // namespace gmx
 
