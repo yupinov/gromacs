e5db3f6aa23eedc6db551c0990541135752caae3
Aleksei Iupinov <a.yupinov@gmail.com>
2018-02-20 17:00:21 +0100

    Add FIXME warp_size define
    Change-Id: I0181c806cfd26287b8178cc0b6cccfc365b3bc54

--- src/gromacs/ewald/pme-types-ocl.h
+++ src/gromacs/ewald/pme-types-ocl.h
@@ -53,6 +53,7 @@
 #include "gromacs/gpu_utils/gputraits_ocl.h"
 
 //#include "gromacs/gpu_utils/cuda_arch_utils.cuh" // for warp_size
+#define warp_size 32 //FIXME this is for PME OpenCL only!
 
 #include "pme-gpu-internal.h"                    // for the general PME GPU behaviour defines
 #include "pme-timings-ocl.h"
